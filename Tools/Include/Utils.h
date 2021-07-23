48a87fea367822633d259242cf7f50c6084046bb
林心淇 <linxinqi@tju.edu.cn>
2021-07-23 12:00:17 +0800

    add xyz to yaw pitch roll

--- Tools/Include/Utils.h
+++ Tools/Include/Utils.h
@@ -2,8 +2,13 @@
 #define __UTILL_H_
 
 #include <opencv2/core/core.hpp>
+#include <cmath>
+
+const double pi = CV_PI;
 
 double getPointLength(const cv::Point2f &p);
 
+double get_pitch(float x, float y, float z);
+double get_yaw(float x, float z);
 
 #endif
--- Tools/Src/Utils.cpp
+++ Tools/Src/Utils.cpp
@@ -3,3 +3,28 @@
 double getPointLength(const cv::Point2f &p) {
     return sqrt(p.x * p.x + p.y * p.y);
 }
+
+double get_pitch(float x, float y, float z) {
+    return atan(y / sqrt(x * x + z * z));
+}
+
+double get_yaw(float x, float z) {
+    if (x == 0 && z == 0)
+        return 0;
+    else if (x == 0 && z < 0)
+        return 0;
+    else if (x == 0 && z > 0)
+        return 180;
+    else if (z == 0 && x > 0)
+        return 90;
+    else if (z == 0 && x < 0)
+        return -90;
+    else if (x > 0 && z > 0)
+        return pi - atan(x / z);
+    else if (x > 0 && z < 0)
+        return atan(-x / z);
+    else if (x < 0 && z < 0)
+        return -atan(x / z);
+    else
+        return -(pi - atan(-x / z));
+}
